22dea20808741d5580baec952a161a7e9e04c8e0
priscillapiloto <priscillaferreira0307@gmail.com>
2025-02-26 08:27:50 -0300

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,68 +1,68 @@
 #include <stdio.h>
 
-// Definição da estrutura para armazenar os dados da carta
-typedef struct {
+int main() {
+
+//TESTE PRISCILLA
+
     char estado;
-    char codigo[4];
+    char codigo[10];
     char nomeCidade[50];
     int populacao;
     float area;
     float pib;
     int pontosTuristicos;
-} Carta;
 
-int main() {
     Carta carta1, carta2;
     
     // Coletando dados da primeira carta
-    printf("Digite o estado da primeira carta: ");
+    printf("Digite o estado da primeira carta:\n ");
     scanf(" %c", &carta1.estado);
     
-    printf("Digite o código da primeira carta: ");
+    printf("Digite o código da primeira carta:\n ");
     scanf("%s", carta1.codigo);
     
-    printf("Digite o nome da cidade da primeira carta: ");
-    scanf(" %49[^"]", carta1.nomeCidade);
+    printf("Digite o nome da cidade da primeira carta:\n ");
+    scanf("%s", carta1.nomeCidade);
     
-    printf("Digite a população da primeira carta: ");
+    printf("Digite a população da primeira carta: \n");
     scanf("%d", &carta1.populacao);
     
-    printf("Digite a área da primeira carta (em km²): ");
+    printf("Digite a área da primeira carta (em km²): \n");
     scanf("%f", &carta1.area);
     
-    printf("Digite o PIB da primeira carta (em bilhões de reais): ");
+    printf("Digite o PIB da primeira carta (em bilhões de reais):\n ");
     scanf("%f", &carta1.pib);
     
-    printf("Digite o número de pontos turísticos da primeira carta: ");
+    printf("Digite o número de pontos turísticos da primeira carta: \n ");
     scanf("%d", &carta1.pontosTuristicos);
 
     // Coletando dados da segunda carta
-    printf("\nDigite o estado da segunda carta: ");
+    printf("\nDigite o estado da segunda carta: \n");
     scanf(" %c", &carta2.estado);
     
-    printf("Digite o código da segunda carta: ");
+    printf("Digite o código da segunda carta: \n");
     scanf("%s", carta2.codigo);
     
-    printf("Digite o nome da cidade da segunda carta: ");
-    scanf(" %49[^"]", carta2.nomeCidade);
+    printf("Digite o nome da cidade da segunda carta: \n");
+    scanf("%s", carta2.nomeCidade);
     
-    printf("Digite a população da segunda carta: ");
+    printf("Digite a população da segunda carta:\n ");
     scanf("%d", &carta2.populacao);
     
-    printf("Digite a área da segunda carta (em km²): ");
+    printf("Digite a área da segunda carta (em km²): \n");
     scanf("%f", &carta2.area);
     
-    printf("Digite o PIB da segunda carta (em bilhões de reais): ");
+    printf("Digite o PIB da segunda carta (em bilhões de reais): \n");
     scanf("%f", &carta2.pib);
     
-    printf("Digite o número de pontos turísticos da segunda carta: ");
+    printf("Digite o número de pontos turísticos da segunda carta: \n");
     scanf("%d", &carta2.pontosTuristicos);
 
     // Exibindo os dados coletados
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", carta1.estado);
-    printf("Código: %s\n", carta1.codigo);
-    printf("Nome da Cidade: %s\n", carta1.nomeCidade);
+    printf("Código: %s", carta1.codigo);
+    printf("Nome da Cidade: %s", carta1.nomeCidade);
     printf("População: %d\n", carta1.populacao);
     printf("Área: %.2f km²\n", carta1.area);
     printf("PIB: %.2f bilhões de reais\n", carta1.pib);
@@ -70,8 +70,7 @@ int main() {
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", carta2.estado);
-    printf("Código: %s\n", carta2.codigo);
-    printf("Nome da Cidade: %s\n", carta2.nomeCidade);
+    printf("Código: %s", carta2.codigo - Nome da Cidade %s, carta2.nomeCidade);
     printf("População: %d\n", carta2.populacao);
     printf("Área: %.2f km²\n", carta2.area);
     printf("PIB: %.2f bilhões de reais\n", carta2.pib);
